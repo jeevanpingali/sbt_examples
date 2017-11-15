de9cb4ccf1ab3424e3e6a37e42ac77e7d7ea1045
Jeevan Pingali <M1035518@mindtree.com>
2017-11-15 15:01:34 +0530

    Initial commit...

---
+++ src/main/scala/HelloAkkaActorSystem.scala
@@ -0,0 +1,9 @@
+package com.packt.chapter1
+
+import akka.actor.ActorSystem
+
+object HelloAkkaActorSystem extends App {
+  val actorSystem = ActorSystem("HelloAkka")
+  println(actorSystem)
+  actorSystem.terminate()
+}
